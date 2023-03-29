d05cb7af7f3c703f9439709fafe7e1a5a8fccb7e
DanielVP26 <daniel.villega3000@gmail.com>
2023-03-29 14:23:34 -0400

    Agregada la dependencia de Tailwind y el archivo de configuración

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,9 @@
 function App() {
-
   return (
-    <div className="App">
+    <div className="p-9 text-3xl font-bold flex justify-center ">
       <h1>PROBANDO MI PRIMER COMMIT</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
