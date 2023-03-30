8bb6323a0141b62b9703fcf186ef46bcc04ed6c2
DanielVP26 <daniel.villega3000@gmail.com>
2023-03-30 17:48:52 -0400

    Added homepage component

---
+++ front-end/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <>
+      <div className="absolute bottom-0 flex justify-around w-full p-4 bg-gray-200">
+        <i className="text-2xl fa-solid fa-house"></i>
+        <i className="text-2xl fa-regular fa-credit-card"></i>
+        <i className="text-2xl fa-regular fa-chart-bar"></i>
+        <i className="text-2xl fa-regular fa-user"></i>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
