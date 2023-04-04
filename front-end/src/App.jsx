615fb9df2948d29131073de041e4f73258e85781
Alvis <alvis.cm@gmail.com>
2023-04-03 19:11:50 -0500

    install react-router-dom

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,19 @@
 import Register from "./pages/Register";
 import Homepage from "./pages/Homepage";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <Register/>
-      <Homepage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage/>} />
+        <Route path="/register" element={<Register/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
