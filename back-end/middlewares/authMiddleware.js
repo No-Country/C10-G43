d1a7e688d7427f24ac1cbfd9bf74161b49558531
DanielVP26 <daniel.villega3000@gmail.com>
2023-04-18 13:10:34 -0400

    hotfix

--- back-end/middlewares/authMiddleware.js
+++ back-end/middlewares/authMiddleware.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 const getIdUser = async (req, res, next) => {
   try {
-    const token = req.headers.authorization; //.split(" ")[1];
+    const token = req.headers.authorization.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.body.userId = decoded.userId;
   } catch (error) {
